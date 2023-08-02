5bc84e5f055c8e6acf698a4ffd33b0654dbef1b8
Paul Prescod <paul@prescod.net>
2023-08-02 07:42:54 -0700

    Clamp both metrics

--- metric_engines/validation_error_metric.py
+++ metric_engines/validation_error_metric.py
@@ -53,30 +53,13 @@ class MetricEngine:
 
         # Calculate the score based on the percent of correct tags
         total_errors = num_wf_errors + num_dtd_errors
-        good_elements  = total_elements - total_errors
 
-        # in case any elements generated multiple errors, we might have a negative number
-        good_elements = max(0, good_elements)
+        goodness  = total_elements - total_errors
 
-        good_tags_ratio = good_elements / total_elements
-        # flip it back to a zero to 100 score with better scores being better
-        res = good_tags_ratio * 100
-        return res
-
-# Test code
-if __name__ == "__main__":
-    # Instantiate the MetricEngine class
-    metric_engine = MetricEngine()
+        good_tags_ratio = goodness / total_elements
+        good_tags_ratio = clamp(good_tags_ratio, 0, 1)
+        validation_error_rate = good_tags_ratio * 100
+        return validation_error_rate
     
-    # Test 1: Well-formed XML with no DTD errors
-    well_formed_xml = ['<root>', '<child>content</child>', '</root>']
-    print(metric_engine.calculate(well_formed_xml, []))  # Should output 0
-    
-    # Test 2: Non-well-formed XML
-    non_well_formed_xml = ['<root>', '<child>content</child>']
-    print(metric_engine.calculate(non_well_formed_xml, []))  # Should output a value <= 0.5
-    
-    # Test 3: Well-formed XML with DTD errors
-    xml_with_dtd_errors = ['<!DOCTYPE root [', '<!ELEMENT root (child)>', '<!ELEMENT child ANY>', ']>', '<root>', '<child>content<child></child></child><extra>error</extra>', '</root>']
-    # Note: For this test case, you would need an appropriate DTD to actually test DTD validation errors.
-    print(metric_engine.calculate(xml_with_dtd_errors, []))
+def clamp(number, bottom, top):
+    return max(bottom, min(number, top))
--- metric_engines/xater_metric.py
+++ metric_engines/xater_metric.py
@@ -7,6 +7,7 @@ from metric_engines.types import MetricInput
 
 
 class MetricEngine:
+
     unit = "%"
 
     def calculate(self, input: MetricInput, output_file_dir: Path) -> float:
@@ -17,16 +18,15 @@ class MetricEngine:
                 fromfile="hypothesis",
                 tofile="reference",
             )
-        (output_file_dir / "hypothesis_tokenized.txt").write_text(
-            "\n".join(input.hypothesis_tokens)
-        )
-        (output_file_dir / "referenced_tokenized.txt").write_text(
-            "\n".join(input.reference_tokens)
-        )
+        (output_file_dir / "hypothesis_tokenized.txt").write_text("\n".join(input.hypothesis_tokens))
+        (output_file_dir / "referenced_tokenized.txt").write_text("\n".join(input.reference_tokens))
         cdiff = "\n".join(diffs)
         output_file_path = output_file_dir / "unified_diff.txt"
         output_file_path.write_text(cdiff)
         with input.profile_logger.log_time("xater.pyter"):
-            return (
-                100 - pyter.ter(input.hypothesis_tokens, input.reference_tokens) * 100
-            )
+            ter = pyter.ter(input.hypothesis_tokens, input.reference_tokens)
+            clamped_ter = clamp(ter, 0, 1)
+            return 100 - clamped_ter * 100
+
+def clamp(number, bottom, top):
+    return max(bottom, min(number, top))
